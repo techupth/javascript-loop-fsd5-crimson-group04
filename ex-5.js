13a39af531b050a2650d6be966cdca992a84b58f
Ping <ping15512@gmail.com>
2023-12-01 16:09:12 +0700

    feat:add answer

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
+let i = 1;
 
-// Start coding here
+for (let techup in companyName) {
+  console.log(`Number ${i} character is ${companyName[techup]}`);
+  i++;
+}
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let i = companyName.length - 1; i >= 0; i = i - 1) {
+  reversedCompanyName += companyName[i];
+}
 
 console.log(reversedCompanyName);
