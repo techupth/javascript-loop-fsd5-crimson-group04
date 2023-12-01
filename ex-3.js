860ec6ffffaef020ff45b01e765db07e7ed52383
nongparanai <promlerd.p@gmail.com>
2023-12-01 15:03:44 +0700

    fix(ex-3): fix code

--- ex-3.js
+++ ex-3.js
@@ -5,7 +5,8 @@ let minNumber;
 minNumber = numbers[0];
 
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] < minNumber) minNumber = numbers[i];
+  if (numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  }
 }
-
 console.log(minNumber);
